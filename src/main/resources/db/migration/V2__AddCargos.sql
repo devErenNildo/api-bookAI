4a91a04c43a9d22ff31c4f44f90dd9832c74422d
eren <dev.erenildo@gmail.com>
2025-06-03 18:06:26 -0300

    autenticacao e autorizacao

--- src/main/resources/db/migration/V2__AddCargos.sql
+++ src/main/resources/db/migration/V2__AddCargos.sql
@@ -1,2 +1,2 @@
-INSERT INTO cargo (cargo) VALUES ('USER');
-INSERT INTO cargo (cargo) VALUES ('ADMIN');
+INSERT INTO tb_roles (role_id, name) VALUES (1, 'admin');
+INSERT INTO tb_roles (role_id, name) VALUES (2, 'basic');
